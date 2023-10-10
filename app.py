883a8272770aab6936065e5e1526fdf6a278d3c7
marco cobian <marco.cobian@coppel.com>
2023-10-09 22:27:54 -0600

    Added the POST method to the UnitMeasure model

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from chalice import Chalice
-from chalicelib.controllers.unit_measure_controller import list_unit_measure
+from chalice import Chalice, Response
+from chalicelib.controllers.unit_measure_controller import list_unit_measure, create_unit_measure
 
 app = Chalice(app_name='challenge')
 
@@ -12,3 +12,21 @@ def index():
 @app.route('/unit-measure', methods=['GET'])
 def get_all_unit_measure():
     return {"data": list_unit_measure()}
+
+
+@app.route('/unit-measure', methods=['POST'])
+def get_all_unit_measure():
+    data = app.current_request.json_body
+    name = data.get('name')
+    if not (name):
+        return Response(
+            body={'error': 'Name factor are required.'},
+            status_code=400,
+        )
+
+    response_data, status_code = create_unit_measure(name=name)
+    return Response(
+        body=response_data,
+        status_code=status_code,
+    )
+
--- chalicelib/controllers/unit_measure_controller.py
+++ chalicelib/controllers/unit_measure_controller.py
@@ -3,18 +3,36 @@ from chalicelib.models.unit_measure_model import UnitMeasure, Session
 
 def create_unit_measure(
     name: str,
-) -> dict:
-    return "test"
+) -> dict and int:
+    try:
+        session = Session()
+        unit_measure = UnitMeasure(name=name)
+        session.add(unit_measure)
+        session.commit()
+        session.refresh(unit_measure)
+        session.close()
+        return {
+            "message": "Unit Measure created successfully.",
+            "data": {
+                "id": unit_measure.id,
+                "name": unit_measure.name
+            }
+        }, 201
+    except Exception as e:
+        return {
+            "message": str(e),
+            "error": "Internal Server Error"
+        }, 500
 
 
 def list_unit_measure() -> list:
     get_all_unit_measure = list()
     session = Session()
-    resultados = session.query(UnitMeasure).all()
-    for resultado in resultados:
+    get_unit_measure = session.query(UnitMeasure).all()
+    for item in get_unit_measure:
         get_all_unit_measure.append({
-            "id": resultado.id,
-            "name": resultado.name
+            "id": item.id,
+            "name": item.name
         })
     session.close()
     return get_all_unit_measure
